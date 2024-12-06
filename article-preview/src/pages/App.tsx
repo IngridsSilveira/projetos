1abe1a6fb1124ac667dc6488749809d2cb00d14f
Ingrid Souza <ingridsouzaok@gmail.com>
2024-12-06 10:34:35 -0300

    dist criada para o article preview

--- article-preview/src/pages/App.tsx
+++ article-preview/src/pages/App.tsx
@@ -1,10 +1,9 @@
-import imgDrawers from "/projetos/article-preview/images/drawers.jpg";
 import { Card } from "./Card/Card";
 function App() {
   return (
     <main className="bg-slate-200 p-10 w-screen min-h-screen flex flex-col md:flex-row items-center justify-center">
       <img
-        src={imgDrawers}
+        src="/projetos/article-preview/images/drawers.jpg"
         alt=""
         className="w-full max-w-96 h-auto min-h-[20rem] md:min-h-[30rem] rounded-tl-lg rounded-tr-lg md:rounded-tr-none md:rounded-bl-lg "
       />
